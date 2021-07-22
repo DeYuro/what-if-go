42d7a04ab5b4fe88f3cf89c6e81ca455af471ae7
Denis Shashkov <dshashkov@beget.ru>
2021-07-22 19:21:30 +0300

    get from slice

--- slice/slice.go
+++ slice/slice.go
@@ -3,23 +3,46 @@ package main
 import "fmt"
 
 func main()  {
-	emptySlice()
-	emptySliceWithCapacity()
+	empty()
+	emptyWithCapacity()
+	getElemInEmpty()
+	getElemInEmptyCapacity()
 }
 // emptySlice show what inside empty slice which created via var
-func emptySlice()  {
+func empty()  {
 	var s []int
 	fmt.Printf("Empty slice created via `var`")
-	sliceDescribe(s)
+	describe(s)
 }
 // emptySlice show what inside empty slice which created via var
-func emptySliceWithCapacity()  {
+func emptyWithCapacity()  {
 	s := make([]int, 10)
 	fmt.Printf("Empty slice created via `make`")
-	sliceDescribe(s)
+	describe(s)
 }
 
-// sliceDescribe print slice content, len, cap
-func sliceDescribe(s []int)  {
+// describe print slice content, len, cap
+func describe(s []int)  {
 	fmt.Printf("content %v, len %d, cap %d\n", s, len(s), cap(s))
+}
+
+// getElemInEmpty show what if get el by index from slice created via `var`
+func getElemInEmpty()  {
+	var s []int
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("While getting el from slice created via `var` got panic: %v\n", r)
+		}
+	}()
+	fmt.Println("\nGet elem from slice created via `var`")
+
+	fmt.Println(s[2])
+}
+
+// getElemInEmptyCapacity show what if get el by index from slice created via `make`
+func getElemInEmptyCapacity()  {
+	s := make([]int, 10)
+	fmt.Println("\nGet elem from slice created via `make`")
+	fmt.Println(s[2])
 }
