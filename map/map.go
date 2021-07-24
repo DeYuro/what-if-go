e9fed213d46cca320d58260a219b2d015c6e7eb2
Денис Шашков <droot@MacBook-Pro-Denis.local>
2021-07-24 11:56:34 +0300

    map into func

--- map/map.go
+++ map/map.go
@@ -3,8 +3,20 @@ package main
 import "fmt"
 
 func main()  {
-	forEach()
+	m := map[int]int{1:1,2:2,3:3,4:4,5:5}
+	fmt.Printf("Original: map %v with pointer %p \n", m, &m)
+	byValue(m) //*map[int]int does not support indexing
+	fmt.Printf("After by value func: map %v pointer %p \n", m, &m)
+
+	//forEach()
 }
+// byValue func show what if map passed by value into func
+func byValue(m map[int]int)  {
+	fmt.Printf("Into by value func before change: array %v pointer %p \n", m, &m)
+	m[2] = 10
+	fmt.Printf("Into by value func after change: array %v pointer %p \n", m, &m)
+}
+
 
 // forEach show what happen when iterate over the map
 // SPOILER: random order
